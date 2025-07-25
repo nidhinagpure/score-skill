3e540ce7dfc63ee11c6fbee845fb1215af10caa5
nidhinagpure <nagpurenidhi8@gmail.com>
2025-07-25 16:57:37 +0530

    first init

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/health", (req,res) => {
+    res.status(200).json({message:"Server is running"});
+})
+
+
+const PORT = 5002;
+
+app.listen(PORT,() => {
+    console.log(`Server is runing n ${PORT}`);
+})
