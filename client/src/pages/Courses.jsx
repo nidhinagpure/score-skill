41f310b7fe617efac70cfa9c58696012904827bc
nidhinagpure <nagpurenidhi8@gmail.com>
2025-07-29 14:58:10 +0530

    vite_api_url

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import axios from "axios";
+import { useState } from 'react'
+import { useEffect } from 'react';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+
+    const loadCourses = async () => {
+      const response = await axios.get(`${import.meta.evn.VITE_API_URL}`/courses
+
+      );
+      setCourses(response.data.data);
+    };
+
+    useEffect(() => {
+     loadCourses() 
+    }, []);
+
   return (
-    <>
-    
-    
-    </>
+    <div>
+      
+    </div>
   )
 }
 
